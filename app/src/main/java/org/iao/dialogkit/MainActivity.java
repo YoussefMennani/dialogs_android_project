71de9b6279fb2bf946405925a129c6055723d554
YoussefMennani <you@example.comyoussefmennani1996@gmail.com>
2023-12-14 15:24:36 +0100

    first commit

---
+++ app/src/main/java/org/iao/dialogkit/MainActivity.java
@@ -0,0 +1,21 @@
+package org.iao.dialogkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void AlertDialog(View view){
+        Toast.makeText(this,"hello",Toast.LENGTH_LONG).show();
+
+    }
+}
